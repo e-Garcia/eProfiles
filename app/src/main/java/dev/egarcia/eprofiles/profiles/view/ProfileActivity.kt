1490f551dca5e3a8fe3efbdf63d5e8ded764447e
Erick Garcia <erick@egarcia.dev>
2022-02-06 02:49:19 -0700

    REQ-04: Added support to like a profile

--- app/src/main/java/dev/egarcia/eprofiles/profiles/view/ProfileActivity.kt
+++ app/src/main/java/dev/egarcia/eprofiles/profiles/view/ProfileActivity.kt
@@ -5,10 +5,7 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.currentRecomposeScope
 import androidx.compose.runtime.rememberCoroutineScope
@@ -84,123 +81,130 @@ class ProfileActivity : AppCompatActivity() {
             HorizontalPager(profileViewState.userProfiles.size, state = pagerState) { page ->
                 // Each page's content
                 val user = profileViewState.userProfiles[page]
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier.fillMaxSize()
-                ) {
+                Scaffold(
+                    content = {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center,
+                            modifier = Modifier.fillMaxSize()
+                        ) {
 
-                    for (configItem in profileViewState.profileConfig) {
-                        when (configItem) {
-                            "name" -> {
-                                val title = getString(R.string.title_name)
-                                Text(
-                                    text = "$title ${user.name}",
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(spacing_s),
-                                    textAlign = TextAlign.Center
-                                )
-                            }
-                            "photo" -> {
-                                GlideImage(
-                                    imageModel = user.photo,
-                                    circularReveal = CircularReveal(duration = 250),
-                                    modifier = Modifier
-                                        .padding(spacing_s)
-                                        .width(size_image_l)
-                                        .height(size_image_l),
-                                    failure = {
+                            for (configItem in profileViewState.profileConfig) {
+                                when (configItem) {
+                                    "name" -> {
+                                        val title = getString(R.string.title_name)
                                         Text(
-                                            text = getString(R.string.error_image_failed),
+                                            text = "$title ${user.name}",
                                             modifier = Modifier
                                                 .fillMaxWidth()
                                                 .padding(spacing_s),
                                             textAlign = TextAlign.Center
                                         )
                                     }
-                                )
-                            }
-                            "gender" -> {
-                                val title = getString(R.string.title_gender)
-                                Text(
-                                    text = "$title ${user.gender ?: getString(R.string.error_null)}",
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(spacing_s),
-                                    textAlign = TextAlign.Center
-                                )
-                            }
-                            "about" -> {
-                                val title = getString(R.string.title_about)
-                                Text(
-                                    text = "$title ${user.about ?: getString(R.string.error_null)}",
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(spacing_s),
-                                    textAlign = TextAlign.Center
-                                )
-
-                            }
-                            "school" -> {
-                                val title = getString(R.string.title_school)
-                                Text(
-                                    text = "$title ${user.school ?: getString(R.string.error_null)}",
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(spacing_s),
-                                    textAlign = TextAlign.Center
-                                )
-                            }
-                            "hobbies" -> {
-                                Text(
-                                    text = getString(R.string.title_hobbies),
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(
-                                            top = spacing_s,
-                                            start = spacing_s,
-                                            end = spacing_s
-                                        ),
-                                    textAlign = TextAlign.Center
-                                )
-                                if (user.hobbies != null) {
-                                    for (hobby in user.hobbies) {
-                                        Text(
-                                            text = hobby,
+                                    "photo" -> {
+                                        GlideImage(
+                                            imageModel = user.photo,
+                                            circularReveal = CircularReveal(duration = 250),
                                             modifier = Modifier
-                                                .fillMaxWidth(),
+                                                .padding(spacing_s)
+                                                .width(size_image_l)
+                                                .height(size_image_l),
+                                            failure = {
+                                                Text(
+                                                    text = getString(R.string.error_image_failed),
+                                                    modifier = Modifier
+                                                        .fillMaxWidth()
+                                                        .padding(spacing_s),
+                                                    textAlign = TextAlign.Center
+                                                )
+                                            }
+                                        )
+                                    }
+                                    "gender" -> {
+                                        val title = getString(R.string.title_gender)
+                                        Text(
+                                            text = "$title ${user.gender ?: getString(R.string.error_null)}",
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .padding(spacing_s),
                                             textAlign = TextAlign.Center
                                         )
                                     }
-                                } else {
-                                    Text(
-                                        text = getString(R.string.error_null),
-                                        modifier = Modifier
-                                            .fillMaxWidth(),
-                                        textAlign = TextAlign.Center
-                                    )
-                                }
+                                    "about" -> {
+                                        val title = getString(R.string.title_about)
+                                        Text(
+                                            text = "$title ${user.about ?: getString(R.string.error_null)}",
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .padding(spacing_s),
+                                            textAlign = TextAlign.Center
+                                        )
 
+                                    }
+                                    "school" -> {
+                                        val title = getString(R.string.title_school)
+                                        Text(
+                                            text = "$title ${user.school ?: getString(R.string.error_null)}",
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .padding(spacing_s),
+                                            textAlign = TextAlign.Center
+                                        )
+                                    }
+                                    "hobbies" -> {
+                                        Text(
+                                            text = getString(R.string.title_hobbies),
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .padding(
+                                                    top = spacing_s,
+                                                    start = spacing_s,
+                                                    end = spacing_s
+                                                ),
+                                            textAlign = TextAlign.Center
+                                        )
+                                        if (user.hobbies != null) {
+                                            for (hobby in user.hobbies) {
+                                                Text(
+                                                    text = hobby,
+                                                    modifier = Modifier
+                                                        .fillMaxWidth(),
+                                                    textAlign = TextAlign.Center
+                                                )
+                                            }
+                                        } else {
+                                            Text(
+                                                text = getString(R.string.error_null),
+                                                modifier = Modifier
+                                                    .fillMaxWidth(),
+                                                textAlign = TextAlign.Center
+                                            )
+                                        }
+
+                                    }
+                                }
                             }
                         }
+                    },
+                    floatingActionButton = {
+                        FloatingActionButton(
+                            modifier = Modifier
+                                .height(size_xl)
+                                .width(size_xl),
+                            onClick = {
+                                user.id?.let { viewModel.onLike(it) }
+                            }) {
+                            GlideImage(
+                                imageModel = R.drawable.ic_like,
+                                modifier = Modifier
+                                    .width(size_l)
+                                    .height(size_l),
+                                alignment = Alignment.Center
+                            )
+                        }
                     }
-                }
-                FloatingActionButton(
-                    modifier = Modifier
-                        .height(size_xl)
-                        .width(size_xl),
-                    onClick = {
-                        user.id?.let { viewModel.onLike(it) }
-                    }) {
-                    GlideImage(
-                        imageModel = R.drawable.ic_like,
-                        modifier = Modifier
-                            .width(size_l)
-                            .height(size_l),
-                        alignment = Alignment.Center
-                    )
-                }
+                )
+
             }
         }
     }
